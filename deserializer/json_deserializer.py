88992493cf214dbd466209561cc5784a61845793
gestoica <gestoica@ecologylab.net>
2012-07-20 16:26:59 +0000

    - Added collection elements parsing, scalar and composite - Further porting of the java deserializer code

--- deserializer/json_deserializer.py
+++ deserializer/json_deserializer.py
@@ -6,11 +6,17 @@ Created on 09.06.2012
 import json
 #from serializer.simpl_types_scope import SimplTypesScope
 from deserializer.deserializer_utils import *
+from deserializer.pull_deserializer import PullDeserializer
+#from ijson import items
+
+class SimplJsonDeserializer(PullDeserializer):
+    pass
+
 
 def deserialize_from_string(data_string):
     deserialized_obj = json.loads(data_string)
     return deserialized_obj
-    
+
 def deserialize_from_file(filename):
     json_file = open(filename, "r")
     data_string = json_file.read()
@@ -18,38 +24,39 @@ def deserialize_from_file(filename):
     json_file.close()
     return scope
 
-class SimplJsonDeserializer:
-    def __init__(self, scope, json_tree):
-        self.scope = scope
-        self.json_tree = json_tree
-        self.instance = None
-        
-    def start_deserialize(self):
-        name = list(self.json_tree.keys())[0]
-        attrs = self.json_tree[name]
-        self.instance = self.deserialize(name, attrs)
-        
-    def deserialize(self, name, attrs):
-        class_descriptor = self.scope.classDescriptors[name]
-        class_name = class_descriptor.simpl_name
-        self.simpl_class = createClass(class_name)
-        new_instance = getClassInstance(class_name)
-        setattr(new_instance, "simpl_tag_name", class_descriptor.tagName)      
-        if len(attrs) > 0:
-            for key, value in attrs.items():
-                fd = class_descriptor.fieldDescriptors[key];
-                if fd.simpl_type == "scalar":
-                    print("just set attr " + fd.name + ", with the value " + value)
-                    setattr(new_instance, fd.name, value)
-                if fd.simpl_type == "composite":
-                    child_tag_name = self.scope.findClassByFullName(fd.element_class)
-                    setattr(new_instance, fd.name, self.deserialize(child_tag_name, value))
-                if fd.simpl_type == "collection":
-                    current_list = []
-                    child_tag_name = self.scope.findClassByFullName(fd.element_class)
-                    members = value[fd.collection_tag_name]
-                    for member in members:
-                        current_list.append(self.deserialize(child_tag_name, member))
-                    setattr(new_instance, fd.name, current_list)
-        return new_instance
+if False:
+    class SimplJsonDeserializer:
+        def __init__(self, scope, json_tree):
+            self.scope = scope
+            self.json_tree = json_tree
+            self.instance = None
+
+        def start_deserialize(self):
+            name = list(self.json_tree.keys())[0]
+            attrs = self.json_tree[name]
+            self.instance = self.deserialize(name, attrs)
+
+        def deserialize(self, name, attrs):
+            class_descriptor = self.scope.classDescriptors[name]
+            class_name = class_descriptor.simpl_name
+            self.simpl_class = createClass(class_name)
+            new_instance = getClassInstance(class_name)
+            setattr(new_instance, "simpl_tag_name", class_descriptor.tagName)
+            if len(attrs) > 0:
+                for key, value in attrs.items():
+                    fd = class_descriptor.fieldDescriptors[key];
+                    if fd.simpl_type == "scalar":
+                        print("just set attr " + fd.name + ", with the value " + value)
+                        setattr(new_instance, fd.name, value)
+                    if fd.simpl_type == "composite":
+                        child_tag_name = self.scope.findClassByFullName(fd.element_class)
+                        setattr(new_instance, fd.name, self.deserialize(child_tag_name, value))
+                    if fd.simpl_type == "collection":
+                        current_list = []
+                        child_tag_name = self.scope.findClassByFullName(fd.element_class)
+                        members = value[fd.collection_tag_name]
+                        for member in members:
+                            current_list.append(self.deserialize(child_tag_name, member))
+                        setattr(new_instance, fd.name, current_list)
+            return new_instance
 
--- deserializer/xml_deserializer.py
+++ deserializer/xml_deserializer.py
@@ -39,6 +39,12 @@ class SimplXmlDeserializer(PullDeserializer):
     def nextEvent(self):
         while True:
             self.current_event, self.current_node = self.pull_events.getEvent()
+            if self.current_event == pulldom.CHARACTERS:
+                nodeText = self.current_node.wholeText
+                nodeText = nodeText.replace(' ','')
+                nodeText = nodeText.replace('\n', '')
+                if nodeText == "":
+                    continue
             if self.current_event == pulldom.START_DOCUMENT or \
                 self.current_event == pulldom.START_ELEMENT or \
                 self.current_event == pulldom.END_ELEMENT or \
@@ -51,9 +57,9 @@ class SimplXmlDeserializer(PullDeserializer):
         if self.current_event == pulldom.START_DOCUMENT:
             self.nextEvent()
         self.rootTag = self.getTagName()
-        self.root = self.getObjectModel(self.rootTag)
+        self.root = self.getObjectModel(self.rootTag, self.rootTag)
 
-    def getObjectModel(self, name):
+    def getObjectModel(self, name, rootName):
         class_descriptor = self.scope.classDescriptors[name]
         class_name = class_descriptor.simpl_name
         self.simpl_class = createClass(class_name)
@@ -67,12 +73,12 @@ class SimplXmlDeserializer(PullDeserializer):
         self.currentFileDescriptor = None
 
         while self.current_event != pulldom.END_DOCUMENT and \
-                (self.current_event != pulldom.END_ELEMENT or (not self.rootTag == self.getTagName())):
+                (self.current_event != pulldom.END_ELEMENT or (not rootName == self.getTagName())):
             if self.current_event != pulldom.START_ELEMENT:
                 if self.current_event == pulldom.CHARACTERS:
                     self.xmlText += self.current_node.wholeText
                 elif self.current_event == pulldom.END_ELEMENT:
-                    pass #to implement !!!!!!!!!!
+                    name = self.current_collection
                 self.nextEvent()
                 continue
 
@@ -83,6 +89,8 @@ class SimplXmlDeserializer(PullDeserializer):
                 self.deserializeScalar(root, self.current_field_descriptor)
             elif self.current_field_descriptor.getType() == FieldType.COMPOSITE_ELEMENT:
                 self.deserializeComposite(root, self.current_field_descriptor)
+            elif self.current_field_descriptor.getType() == FieldType.COLLECTION_ELEMENT:
+                self.deserializeCompositeCollection(self, self.current_field_descriptor)
             else:
                 self.nextEvent()
         return root
@@ -91,6 +99,8 @@ class SimplXmlDeserializer(PullDeserializer):
     def deserializeAttributes(self, root, attrs, class_descriptor):
         if len(attrs) > 0:
             for key, value in attrs.items():
+                if key == "xmlns:simpl":
+                    continue
                 fd = class_descriptor.fieldDescriptors[key];
                 if fd.isScalarTag():
                     print("just set attr " + fd.name + ", with the value " + value)
@@ -110,11 +120,32 @@ class SimplXmlDeserializer(PullDeserializer):
     def deserializeComposite(self, parent, field_descriptor):
         tag = self.getTagName()
         class_name = self.scope.findClassByFullName(field_descriptor.element_class)
-        subRoot = self.getObjectModel(class_name)
+        subRoot = self.getObjectModel(class_name, tag)
         setattr(parent, tag, subRoot)
-        
-    def deserializeCompositeCollection(self, parent, field_descriptor):
-        pass
+        self.nextEvent()
+
+    def deserializeCompositeCollection(self, parent, fd):
+        #fd = class_descriptor.collectionFieldDescriptors[node_name]
+        if hasattr(parent, fd.name):
+            current_list = getattr(parent, fd.name)
+        else:
+            current_list = []
+        self.current_collection = fd.name
+        #self.nextEvent()
+        tagName = self.getTagName()
+        while fd.isCollectionTag(tagName):
+            if self.current_event != pulldom.START_ELEMENT:
+                break
+            else:
+                child_tag_name = self.scope.findClassByFullName(fd.element_class)
+                subRoot = self.getObjectModel(child_tag_name, tagName)
+                current_list.append(subRoot)
+                setattr(parent, fd.name, current_list)
+
+                self.nextEvent()
+                tagName = self.getTagName()
+
+        return self.current_event
 
     def deserializeCompositeMap(self, parent, field_descriptor):
         pass
@@ -128,73 +159,3 @@ class SimplXmlDeserializer(PullDeserializer):
             return node.prefix + ":" + node.localName
         else:
             return node.localName
-
-
-
-if False:
-    def deserialize_xml_from_string(string):
-        return ElementTree.fromstring(string)
-
-    def deserialize_xml_from_file(filename):
-        xml_file = open(filename, "r")
-        data_string = xml_file.read()
-        scope = deserialize_xml_from_string(data_string)
-        xml_file.close()
-        return scope
-
-    class SimplXmlDeserializer:
-        def __init__(self, scope, xml_tree):
-            self.scope = scope
-            self.xml_tree = xml_tree
-            self.instance = None
-
-        def start_deserialize(self):
-            name = self.xml_tree.tag
-            attrs = self.xml_tree.attrib
-            child_nodes = list(self.xml_tree)
-            self.instance = self.deserialize(name, attrs, child_nodes)
-
-        def deserialize(self, name, attrs, child_nodes):
-            class_descriptor = self.scope.classDescriptors[name]
-            class_name = class_descriptor.simpl_name
-            self.simpl_class = createClass(class_name)
-            new_instance = getClassInstance(class_name)
-            setattr(new_instance, "simpl_tag_name", class_descriptor.tagName)
-            if len(attrs) > 0:
-                for key, value in attrs.items():
-                    fd = class_descriptor.fieldDescriptors[key];
-                    if fd.simpl_type == "scalar":
-                        print("just set attr " + fd.name + ", with the value " + value)
-                        setattr(new_instance, fd.name, value)
-            if len(child_nodes) > 0:
-                for node in child_nodes:
-                    node_name = node.tag
-                    if node_name in class_descriptor.fieldDescriptors:
-                        fd = class_descriptor.fieldDescriptors[node_name];
-                        if fd.simpl_type == "scalar":
-                            value = node.text
-                            print("just set attr " + fd.name + ", with the value " + value)
-                            setattr(new_instance, fd.name, value)
-                        if fd.simpl_type == "composite":
-                            child_tag_name = self.scope.findClassByFullName(fd.element_class)
-                            setattr(new_instance, fd.name, self.deserialize(child_tag_name, node.attrib, list(node)))
-
-                    #nowrap collections
-                    elif node_name in class_descriptor.collectionFieldDescriptors:
-                        fd = class_descriptor.collectionFieldDescriptors[node_name]
-                        if hasattr(new_instance, fd.name):
-                            current_list = getattr(new_instance, fd.name)
-                        else:
-                            current_list = []
-                        child_tag_name = self.scope.findClassByFullName(fd.element_class)
-                        current_list.append(self.deserialize(child_tag_name, node.attrib, list(node)))
-                        setattr(new_instance, fd.name, current_list)
-                        if False:
-                            child_tag_name = self.scope.findClassByFullName(fd.element_class)
-                            members = value[fd.collection_tag_name]
-                            for member in members:
-                                current_list.append(self.deserialize(child_tag_name, member))
-                            setattr(new_instance, fd.name, current_list)
-            #if len(child_nodes) > 0:
-
-            return new_instance
