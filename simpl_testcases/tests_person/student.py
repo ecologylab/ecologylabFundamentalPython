9b8fae3e37718efbc6fe57eed5d582ee25cb938b
gestoica <gestoica@ecologylab.net>
2012-08-07 15:38:13 +0000

    added tests for inheritance and polymorphism

---
+++ simpl_testcases/tests_person/student.py
@@ -0,0 +1,44 @@
+'''
+Created on 04.06.2012
+
+@author: cristi
+'''
+import unittest
+from serializer.simpl_types_scope import SimplTypesScope
+from serializer.xml_serializer import prettify, XmlSimplSerializer
+from simpl_testcases.tests import testing_utils
+from deserializer.json_deserializer import deserialize_from_file
+from simpl_testcases.tests import testing_utils
+from xml.etree import ElementTree
+from xml.etree.ElementTree import tostring
+import json
+
+class Point(unittest.TestCase):
+
+    def setUp(self):
+        self.scope = SimplTypesScope("JSON", "student_scope")
+        self.pointXMLResult = open("student.xml", "r").read()
+        
+    def test_xml_run(self):
+        simpl_object = self.scope.deserialize("student.xml", "XML")
+        if False:
+            xmlelement = self.scope.serialize(simpl_object, "XML")
+            print(prettify(xmlelement))
+            
+            expected_result = self.pointXMLResult
+            print(expected_result)
+            
+            self.assertTrue(testing_utils.xml_compare(xmlelement, ElementTree.fromstring(expected_result)))
+
+    def test_json_run(self):
+        simpl_object = self.scope.deserialize("student.json", "JSON")
+        if False:    
+            json_text = deserialize_from_file("student.json")
+            print(json_text)
+            json_element = self.scope.serialize(simpl_object, "JSON")
+            
+            print (json.dumps(json_element))
+            self.assertTrue(json_text, json.dumps(json_element))
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
